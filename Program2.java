f12cca68952a955390fb2bb520dc24f145f1abe3
RajiiM <93920019+RajiiM@users.noreply.github.com>
2021-11-20 21:45:35 +0530

    Updated

--- Program2.java
+++ Program2.java
@@ -6,3 +6,13 @@ public class Program2 {
 		Program1 prgm1_obj2 = new Program1(30, 30);
 	}
 }
+
+
+/*
+30
+60
+
+
+In Program1, zero argument constructor is private. So, we can't create zero argument object from Program2 class.
+In Program1, single & double argument constructor is not private. So, we can create single & double argument object from Program2 class.
+*/
